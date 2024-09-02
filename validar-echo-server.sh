1a190943497210bf0979b1f4e928c3f425bb9bc4
Manuel <jpol@fi.uba.ar>
2024-09-02 15:40:43 -0300

    cambio en sintaxis del validar-echo-server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,7 +2,7 @@
 
 TEST_MESSAGE="Mensaje de prueba"
 
-RESULT=$(docker run --rm --network tp0_testing_net busybox:latest sh -c "echo ${TEST_MESSAGE} | nc server 12345")
+RESULT=$(docker run --rm --network tp0_testing_net busybox:latest sh -c "echo '$TEST_MESSAGE' | nc server 12345")
 
 if [ "$RESULT" == "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
